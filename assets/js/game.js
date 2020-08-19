3dd6509b4dc69ba125091c821ff86ab202a0bbba
ginomaglaque <ginomaglaque@gmail.com>
2020-08-18 17:49:15 -0700

    created for loop to loop through ech enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,22 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//    * Fight all enemy robots
+//    * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){
+console.log(enemyNames.length);
+var fight = function(enemyName){
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -61,5 +67,7 @@ var fight = function(){
     }
 }
 
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+};
 
-fight();
